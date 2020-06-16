789b0d979d159fafb7cfeacf1bca438f6ef2c7a8
sulemigoreng <sulemigoreng@gmail.com>
2020-06-16 21:29:13 +0700

    Test Case 7 8 dan 9

---
+++ src/test/java/com/id/perpus/TC7Test.java
@@ -0,0 +1,91 @@
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TC7Test {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testTC7() throws Exception {
+    driver.get("http://localhost:8080/login");
+    driver.findElement(By.id("username")).click();
+    driver.findElement(By.id("username")).clear();
+    driver.findElement(By.id("username")).sendKeys("susi@email.com");
+    driver.findElement(By.id("password")).click();
+    driver.findElement(By.id("password")).clear();
+    driver.findElement(By.id("password")).sendKeys("123456");
+    driver.findElement(By.id("btnLogin")).click();
+    driver.findElement(By.linkText("Transaction")).click();
+    driver.findElement(By.xpath("//li[@id='BFB20002']/a/span")).click();
+    driver.findElement(By.id("memberName")).click();
+    driver.findElement(By.id("memberName")).clear();
+    driver.findElement(By.id("memberName")).sendKeys("Budi");
+    driver.findElement(By.id("majors")).click();
+    driver.findElement(By.id("majors")).clear();
+    driver.findElement(By.id("majors")).sendKeys("asdkjaskldajslk");
+    driver.findElement(By.id("btnSearch")).click();
+    try {
+      assertFalse(driver.findElement(By.id("tableArea")).isDisplayed());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    // ERROR: Caught exception [unknown command []]
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
