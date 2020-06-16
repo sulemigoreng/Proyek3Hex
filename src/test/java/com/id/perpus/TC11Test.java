179717ab0c8d40b8f009af60b0e11088b2ccb3b3
sulemigoreng <sulemigoreng@gmail.com>
2020-06-16 22:29:26 +0700

    Memperbaiki Tio S2

--- src/test/java/com/id/perpus/TC11Test.java
+++ src/test/java/com/id/perpus/TC11Test.java
@@ -6,7 +6,7 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class TC11Test {
@@ -17,7 +17,7 @@ public class TC11Test {
 
   @Before
   public void setUp() throws Exception {
-    driver = new FirefoxDriver();
+    driver = new ChromeDriver();
     baseUrl = "https://www.google.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
